07980f7fbf1c9822112d631754165c3b334da85a
swimmingcoach <zshang_0630@sina.com>
2017-11-09 21:20:04 +0800

    create

--- test3.py
+++ test3.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def add_layer(inputs, in_size, out_size, activation_function=None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
@@ -15,7 +16,7 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 
 
 x_data = np.linspace(-1, 1, 300, dtype=np.float32)[:, np.newaxis]
-noise = np.random.normal(0, 0.05, x_data.shape).astype(np.float32)
+noise = np.random.normal(0, 0.02, x_data.shape).astype(np.float32)
 y_data = np.square(x_data) - 0.5 + noise
 
 xs = tf.placeholder(tf.float32, [None, 1])
@@ -37,9 +38,9 @@ sess.run(init)
 
 # plot the real data
 fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
+ax = fig.add_subplot(1, 1, 1)
 ax.scatter(x_data, y_data)
-plt.ion()#本次运行请注释，全局运行不要注释
+plt.ion()  # 本次运行请注释，全局运行不要注释
 plt.show()
 
 for i in range(2000):
